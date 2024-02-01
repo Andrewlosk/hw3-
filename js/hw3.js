75d8dbda358a78976a4cffb8323197f46a8895fe
Andrewlosk <114972263+Andrewlosk@users.noreply.github.com>
2024-02-01 15:41:27 +0200

    Initial commit

---
+++ js/hw3.js
@@ -0,0 +1,82 @@
+'use strict'
+
+// task1
+// console.log((Math.random() * (19 - 1) + 1));
+// console.log(((Math.random() * (10 - 1) + 1)).toFixed());
+
+
+// task2
+// let num1 = prompt('type number1');
+// let num2 = prompt('type number2');
+// num1 = Number(num1);
+// num2 = Number(num2);
+// let sum = num1 + num2;
+// console.log(sum);
+
+
+// task3
+// let сelsius = 20;
+// let fahrenheit = сelsius * 9 / 5 + 32;
+// console.log(fahrenheit);
+
+
+
+// task4
+// let days = 30;
+// let hours = days * 24;
+// let minutes = hours * 60;
+// let seconds = minutes * 3600;
+// console.log(hours, minutes, seconds);
+
+
+
+
+// task5
+// let health = 100; 
+// let energy = 100;
+
+// const headshot = 80;
+// const ultimate = 50;
+
+// health = health - headshot;
+// energy = energy - ultimate;
+
+// console.log(health, energy);
+
+
+
+// task6
+// let summ = 500;
+// percent = summ * 0.1;
+// summ = summ - percent;
+// console.log(summ);
+
+
+// task7
+// let number1 = 1.76;
+// console.log(Math.floor(number1));
+
+
+// task8
+// let number = '3.4342deg';
+// number = Number.parseFloat(number);
+// console.log(number);
+
+
+// task9
+// let int = '3.4342deg';
+// int = Number.parseInt(int);
+// console.log(int);
+
+// task10
+// let numFirst = 9;
+// console.log(Math.sqrt(numFirst));
+
+// task 11
+// let str = '7.2321dpds';
+// let num = 32;
+// str = Number.parseInt(str);
+// console.log(str);
+
+// num = toString(num);
+// console.log(typeof num);
