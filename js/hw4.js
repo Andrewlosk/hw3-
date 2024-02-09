b712b315d3df04c231c203d79407ac7e4df2632b
Andrewlosk <114972263+Andrewlosk@users.noreply.github.com>
2024-02-09 19:10:38 +0200

    commit

---
+++ js/hw4.js
@@ -0,0 +1,31 @@
+'use strict'
+
+
+// 1
+// const result = 5 + 5 + `5`;
+// console.log(result);
+// console.log(typeof result);
+
+
+// 2
+// const email = 'andrijlosk@gmail.com'
+// console.log(email.includes('@'));
+// console.log(email.length);
+
+
+// 3
+// const my = 'My ';
+// const name = 'name ';
+// const is = 'is ';
+// let fullname = my + name + is;
+// console.log(fullname);
+// const viktor = 'Victor';
+// fullname = fullname + viktor;
+// console.log(fullname);
+
+
+// 4
+const userName = 'Andrii';
+const payment = '300';
+
+const message = alert(`Дякуємо, ${userName} ! До сплати ${payment} гривень`);
