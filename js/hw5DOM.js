958f82e9be302282716429cebdf8071586c1647f
Andrewlosk <114972263+Andrewlosk@users.noreply.github.com>
2024-02-18 18:19:16 +0200

    commit DOM

---
+++ js/hw5DOM.js
@@ -0,0 +1,41 @@
+'use strict';
+
+
+// task 1
+const btnFirst = document.getElementById('btnFirst');
+const inpFirst = document.getElementById('inpFirst');
+
+
+btnFirst.addEventListener('click', () => {
+    const text = inpFirst.value;
+    btnFirst.textContent = text;
+
+});
+
+
+
+// task 2
+const img = document.querySelector('.flag');
+
+img.src = 'https://ms.detector.media/doc/images/news/33333/i75_ArticleImage_33333.webp';
+
+
+
+
+
+// task 3
+const imgNew = document.querySelector('.imgNew');
+const link = document.querySelector('.link');
+
+link.href = 'https://www.youtube.com/';
+imgNew.alt = 'like';
+
+
+
+// task 4
+const firstLi = document.querySelector('p');
+
+// console.log(firstLi);
+
+firstLi.textContent = 'new Text';
+
