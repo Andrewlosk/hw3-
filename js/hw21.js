b9543844492cf7e1ef31a3a9482f4de819a7bb80
Andrewlosk <114972263+Andrewlosk@users.noreply.github.com>
2024-06-17 14:29:48 +0300

    commit

---
+++ js/hw21.js
@@ -0,0 +1,102 @@
+const gallery = document.querySelector('.gallery');
+const images = document.querySelectorAll('.image');
+
+let currentImageIndex = 0;
+
+// Додаємо обробник події для нажаття клавіш
+gallery.addEventListener('keydown', (event) => {
+});
+
+
+
+
+
+
+
+
+
+//task 2
+
+const boxes = document.getElementById('boxes');
+const createBtn = document.querySelector('[data-action="render"]');
+const deleteBtn = document.querySelector('[data-action="destroy"]');
+const input = document.querySelector('.input');
+
+let currentWidth = 30;
+let currentHeight = 30;
+
+const createBoxes = (amount) => {
+    for (let i = 0; i < amount; i++){
+        const box = document.createElement('div')
+        box.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+        box.style.width = `${currentWidth}px`
+        box.style.height = `${currentHeight}px`
+
+        boxes.appendChild(box);
+
+        currentHeight += 10;
+        currentWidth += 10;
+    }
+
+}
+
+const destroyBoxes = () => {
+    boxes.innerHTML = '';
+    currentWidth = 30;
+    currentHeight = 30;
+}
+
+
+createBtn.addEventListener('click', () => {
+    const amount = parseInt(input.value);
+    if (amount > 0 && amount <= 100) {
+        createBoxes(amount)
+    }else {
+        alert('введи число від 1 до 100')
+    }
+})
+
+deleteBtn.addEventListener('click', () => {
+    destroyBoxes()
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
